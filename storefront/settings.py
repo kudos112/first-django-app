27b8b52cdba2039cbad4b7a69674b93496c87b7c
Abdul Quddous <quddoux112@gmail.com>
2023-03-26 22:59:30 +0500

    added apps, models, and made migrations

--- storefront/settings.py
+++ storefront/settings.py
@@ -38,7 +38,10 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'playground',
-    'debug_toolbar'
+    'debug_toolbar',
+    'stores',
+    'tags',
+    'likes'
 ]
 
 MIDDLEWARE = [
